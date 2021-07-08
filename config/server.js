cef62b28b7a8e65cd4c3ef53d3e61610f4661be2
Bradley Genao <bjgenao@gmail.com>
2021-07-08 08:18:17 -0700

    Back end ready for delpoyment on heroku

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8bc6bedd5af39f590a197ab4a5bb16ec'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8f7b5d15-406d-4522-9eeb-dc8bb2e0be05'
+};
